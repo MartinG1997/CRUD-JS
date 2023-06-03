cb2e14fe38b2c0b06c6fb657a860261739ab7dca
MartinG1997 <martingrinoh@gmail.com>
2023-06-03 15:11:17 -0400

    Se agrega un poco de estilo y se corrigen formulas finales y error al eliminar

---
+++ README.md
@@ -0,0 +1,58 @@
+
+# Proyecto 2: Aplicación CRUD
+
+
+
+## Índice
+
+* [1. Introducción](#Introducción)
+* [2. Demo](#Demostración)
+* [3. ¿Qué es?](#¿Qué-es?)
+* [4. Objetivos de aprendizaje](#Objetivos-de-aprendizaje)
+* [5. Requisitos](#Requisitos)
+* [6. Agradecimientos](#Agradecimientos)
+## Introducción
+
+Con la finalidad de seguir aprendiendo un poco de desarrollo FullStack, uno se encuentra con los llamados procesos CRUD, operaciones que trabajan con datos para persistirlos en una base de datos. A falta de esta, se utiliza el llamado localStorage, que permite persistir los datos de forma local, o sea, en el navegador del computador del usuario.
+
+Esto sirve para facilitar vistas, búsqueda y modificación de información.
+
+CRUD es un acrónimo para Create, Read, Update, Delete.
+## Demo
+
+Puedes entrar a ver el demo aquí: https://marting1997.github.io/CRUD-JS/
+
+
+En este caso se desarrolló una aplicación para el cálculo del sueldo bruto teniendo el líquido, en la cual se requiere un CRUD.
+
+En ella se tienen los siguientes puntos:
+
+- Se pueden crear personas con sus sueldos con **Guardar**, para ello hay que agregar los campos Nombre, Apellido, Monto y la lista con las AFPs.
+- Si no se agrega ningún registro nuevo, con el botón **Cargar** se cargarán todos los datos almacenados en localStorage.
+- Con el botón **Editar** se cargarán en los campos superiores los datos básicos y se podrá actualizar dicha información con el botón **Actualizar**.
+-  Borrar registro o tabla completa, con el botón **Eliminar** se eliminará el registro seleccionado y con **Limpiar** se borrarán todos los registros.
+
+## ¿Qué es?
+
+Como se mencionó , lo que busca esta aplicación es calcular a raíz del sueldo líquido el bruto. Para ello hay que tener ciertas nociones.
+
+- Lo primero que hay que tener en consideración es que este proyecto está pensando para los sueldos de Chile.
+- Posterior a ello, en Chile existen los siguientes descuentos legales: Seguro de cesantía, FONASA (Salud), AFPs (Sistema de ahorro para pensión.)
+- Por último, estos descuentos tienen valores asociados. El seguro de cesantía para trabajadores antiguos es del 0.6%, FONASA del 7% y la AFP varía según en el cual el trabajador esté inscrito.
+## Objetivos de aprendizaje
+
+- Aplicar operaciones CRUD en un segmento de datos.
+- Puesta a prueba de conocimientos base.
+- Extra: Utilizar una API.
+## Requisitos
+
+Esta aplicación puede correr de forma local en un computador, no requiere ninguna descarga de ningún software, además, para su uso estará disponible el link para probarlo sin restricciones.
+## Agradecimientos
+
+Finalmente quiero hacer un especial agredicimientos a:
+
+- **[Bootcamp Desarrollador FullStack UDD](https://github.com/UDDBootcamp)** : Por el buen material de sus cursos y la exigencia que pone para aprobar.
+
+- **[Bryan Diaz C](https://github.com/brayandiazc)** : Profesor del Bootcamp.
+
+- **[Gael Cloud](https://api.gael.cloud/#introduccion)** : Por sus API, en especial para este proyecto la de indicadores previsionales.
--- js/storage.js
+++ js/storage.js
@@ -1,77 +1,94 @@
-$(document).ready(function() 
-{
-    $.getJSON("https://api.gael.cloud/general/public/previred/072022", function(data) 
-    {
-        var indicadores = ["AFPCapitalTasaDep","AFPCuprumTasaDep", "AFPHabitatTasaDep", "AFPPlanVitalTasaDep", "AFPProVidaTasaDep", "AFPModeloTasaDep", "AFPUnoTasaDep"];
-        $.each(data, function(key, value) 
-        {
-            if (indicadores.includes(key)) 
-            {
-                $("#indicadores").append("<option value='" + value + "'>" + key + "</option>");
-            }
-        });
-    });
+$(document).ready(function () {
+    $.getJSON(
+        "https://api.gael.cloud/general/public/previred/072022",
+        function (data) {
+            var indicadores = [
+                "AFPCapitalTasaDep",
+                "AFPCuprumTasaDep",
+                "AFPHabitatTasaDep",
+                "AFPPlanVitalTasaDep",
+                "AFPProVidaTasaDep",
+                "AFPModeloTasaDep",
+                "AFPUnoTasaDep",
+            ];
+            $.each(data, function (key, value) {
+                if (indicadores.includes(key)) {
+                    $("#indicadores").append(
+                        "<option value='" + value + "'>" + key + "</option>"
+                    );
+                }
+            });
+        }
+    );
 });
 
-function SaveData() 
-{
-    var nombre = document.getElementById('nombre').value;
-    var apellido = document.getElementById('apellido').value;
-    var monto = document.getElementById('monto').value;
+function SaveData() {
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var monto = document.getElementById("monto").value;
     const selectList = document.getElementById("indicadores");
     var afp = selectList.options[selectList.selectedIndex].textContent;
-    var porcentajeafp = parseFloat(selectList.value.replace(',', '.'));
-    var montoReal = ((monto*100)/(100-7-porcentajeafp)).toFixed(2);
-    var montoAFP = ((montoReal*porcentajeafp)/100).toFixed(2);
-    var montoFONASA = (montoReal*0.07).toFixed(2);
+    var porcentajeafp = parseFloat(selectList.value.replace(",", "."));
+    var montoReal = ((monto * 100) / (100 - 7 - porcentajeafp - 0.6)).toFixed(2);
+    var montoAFP = ((montoReal * porcentajeafp) / 100).toFixed(2);
+    var montoFONASA = (montoReal * 0.07).toFixed(2);
+    var montoSeguro = (montoReal*0.006).toFixed(2);
 
-    if (nombre && apellido && monto && selectList) 
-    {
-      // Obtener los datos existentes en LocalStorage
-      var datosGuardados = JSON.parse(localStorage.getItem('datos')) || [];
-      // Agregar el nuevo dato al array
-      datosGuardados.push({ nombre: nombre, apellido: apellido, monto: monto, afp: afp, porcentajeafp: porcentajeafp, montoReal: montoReal, montoAFP: montoAFP, montoFONASA: montoFONASA});
-  
-      // Guardar el array actualizado en LocalStorage
-      localStorage.setItem('datos', JSON.stringify(datosGuardados));
-  
-      // Limpiar los campos de entrada
-      document.getElementById('nombre').value = '';
-      document.getElementById('apellido').value = '';
-      document.getElementById('monto').value = '';
-      document.getElementById('indicadores').value = '';
-  
-      // Actualizar la tabla
-      LoadData();
-    }
-    else
-    {
-        alert('Falta asignar valor a uno de los siguientes campos: \n 1. Nombre \n 2. Apellido \n 3. Monto \n 4. Lista de AFPs');
+    if (nombre != "" && apellido != "" && monto != "" && selectList.value != "") {
+        // Obtener los datos existentes en LocalStorage
+        var datosGuardados = JSON.parse(localStorage.getItem("datos")) || [];
+        // Agregar el nuevo dato al array
+        datosGuardados.push({
+            nombre: nombre,
+            apellido: apellido,
+            monto: monto,
+            afp: afp,
+            porcentajeafp: porcentajeafp,
+            montoReal: montoReal,
+            montoAFP: montoAFP,
+            montoFONASA: montoFONASA,
+            montoSeguro: montoSeguro
+        });
+
+        // Guardar el array actualizado en LocalStorage
+        localStorage.setItem("datos", JSON.stringify(datosGuardados));
+
+        // Limpiar los campos de entrada
+        document.getElementById("nombre").value = "";
+        document.getElementById("apellido").value = "";
+        document.getElementById("monto").value = "";
+        document.getElementById("indicadores").value = "";
+
+        // Actualizar la tabla
+        LoadData();
+    } else {
+        alert(
+            "Falta asignar valor a uno de los siguientes campos: \n 1. Nombre \n 2. Apellido \n 3. Monto \n 4. Lista de AFPs"
+        );
     }
 }
-  
-function LoadData() 
-{
-    var datosGuardados = JSON.parse(localStorage.getItem('datos')) || [];
-    var tabla = document.getElementById('tablaDatos');
-    var tbody = tabla.getElementsByTagName('tbody')[0];
-  
-    tbody.innerHTML = '';
-  
-    datosGuardados.forEach(function (dato, index) 
-    {
-        var fila = document.createElement('tr');
-        var celdaNombre = document.createElement('td');
-        var celdaApellido = document.createElement('td');
-        var celdaLiquido = document.createElement('td');
-        var celdaAFP = document.createElement('td');
-        var celdaPorcentajeAfp = document.createElement('td');
-        var celdaMonto = document.createElement('td');
-        var celdaMontoAfp = document.createElement('td');
-        var celdaMontoFonasa = document.createElement('td');
-        var celdaEditar = document.createElement('td');
-        var botonEditar = document.createElement('button');
-        var btnDelete = document.createElement('button');
+
+function LoadData() {
+    var datosGuardados = JSON.parse(localStorage.getItem("datos")) || [];
+    var tabla = document.getElementById("tablaDatos");
+    var tbody = tabla.getElementsByTagName("tbody")[0];
+    tabla.classList.remove("oculto");
+    tbody.innerHTML = "";
+
+    datosGuardados.forEach(function (dato, index) {
+        var fila = document.createElement("tr");
+        var celdaNombre = document.createElement("td");
+        var celdaApellido = document.createElement("td");
+        var celdaLiquido = document.createElement("td");
+        var celdaAFP = document.createElement("td");
+        var celdaPorcentajeAfp = document.createElement("td");
+        var celdaMonto = document.createElement("td");
+        var celdaMontoAfp = document.createElement("td");
+        var celdaMontoFonasa = document.createElement("td");
+        var celdaMontoSeguro = document.createElement("td");
+        var celdaEditar = document.createElement("td");
+        var botonEditar = document.createElement("button");
+        var btnDelete = document.createElement("button");
         //img.setattribute('src', ...);
 
         celdaNombre.textContent = dato.nombre;
@@ -82,22 +99,21 @@ function LoadData()
         celdaMonto.textContent = dato.montoReal;
         celdaMontoAfp.textContent = dato.montoAFP;
         celdaMontoFonasa.textContent = dato.montoFONASA;
-    
-        botonEditar.textContent = 'Editar';
-        botonEditar.addEventListener('click', function () 
-        {
+        celdaMontoSeguro.textContent = dato.montoSeguro;
+
+        botonEditar.textContent = "Editar";
+        botonEditar.addEventListener("click", function () {
             EditData(index); // Pasamos el índice del dato a editar
         });
 
-        btnDelete.textContent = 'Eliminar';
-        btnDelete.addEventListener('click', function () 
-        {
+        btnDelete.textContent = "Eliminar";
+        btnDelete.addEventListener("click", function () {
             DeleteData(index); // Pasamos el índice del dato a editar
         });
-  
+
         celdaEditar.appendChild(botonEditar);
         celdaEditar.appendChild(btnDelete);
-        
+
         fila.appendChild(celdaNombre);
         fila.appendChild(celdaApellido);
         fila.appendChild(celdaLiquido);
@@ -106,104 +122,136 @@ function LoadData()
         fila.appendChild(celdaMonto);
         fila.appendChild(celdaMontoAfp);
         fila.appendChild(celdaMontoFonasa);
+        fila.appendChild(celdaMontoSeguro)
         fila.appendChild(celdaEditar);
-  
+
         tbody.appendChild(fila);
     });
 }
 
-function EditData(index) 
-{
-    var datosGuardados = JSON.parse(localStorage.getItem('datos')) || [];
+function EditData(index) {
+    var datosGuardados = JSON.parse(localStorage.getItem("datos")) || [];
     var dato = datosGuardados[index];
-  
+
     if (dato) {
-      document.getElementById('nombre').value = dato.nombre;
-      document.getElementById('apellido').value = dato.apellido;
-      document.getElementById('monto').value = dato.monto;
+        document.getElementById("nombre").value = dato.nombre;
+        document.getElementById("apellido").value = dato.apellido;
+        document.getElementById("monto").value = dato.monto;
     }
-    document.getElementById('Id').value = index;
+    document.getElementById("Id").value = index;
 }
 
-function DeleteData(index)
-{
-    var datosGuardados = JSON.parse(localStorage.getItem('datos')) || [];
-    delete(datosGuardados[index]);
-    localStorage.setItem('datos', JSON.stringify(datosGuardados));
+function DeleteData(index) {
+    var datosGuardados = JSON.parse(localStorage.getItem("datos")) || [];
+    datosGuardados.splice(index, 1);
+    localStorage.setItem("datos", JSON.stringify(datosGuardados));
     LoadData();
 }
 
-function UpdateData()
-{
+function UpdateData() {
     const selectList = document.getElementById("indicadores");
-    if(selectList.value != '')
-    {
-        index = document.getElementById('Id').value;
-        var datosGuardados = JSON.parse(localStorage.getItem('datos')) || [];
-        var nombre = document.getElementById('nombre').value;
-        var apellido = document.getElementById('apellido').value;
-        var monto = document.getElementById('monto').value;
+    if (selectList.value != "") {
+        index = document.getElementById("Id").value;
+        var datosGuardados = JSON.parse(localStorage.getItem("datos")) || [];
+        var nombre = document.getElementById("nombre").value;
+        var apellido = document.getElementById("apellido").value;
+        var monto = document.getElementById("monto").value;
         var afp = selectList.options[selectList.selectedIndex].textContent;
-        var porcentajeafp = parseFloat(selectList.value.replace(',', '.'));
-        var montoReal = ((monto*100)/(100-7-porcentajeafp)).toFixed(2);
-        var montoAFP = ((montoReal*porcentajeafp)/100).toFixed(2);
-        var montoFONASA = (montoReal*0.07).toFixed(2);
+        var porcentajeafp = parseFloat(selectList.value.replace(",", "."));
+        var montoReal = ((monto * 100) / (100 - 7 - porcentajeafp - 0.6)).toFixed(2);
+        var montoAFP = ((montoReal * porcentajeafp) / 100).toFixed(2);
+        var montoFONASA = (montoReal * 0.07).toFixed(2);
+        var montoSeguro = (montoReal * 0.006).toFixed(2);
 
-        datosGuardados[index]={ nombre: nombre, apellido: apellido, monto: monto, afp: afp, porcentajeafp: porcentajeafp, montoReal: montoReal, montoAFP: montoAFP, montoFONASA: montoFONASA};
-        localStorage.setItem('datos', JSON.stringify(datosGuardados));
-        document.getElementById('nombre').value = '';
-        document.getElementById('apellido').value = '';
-        document.getElementById('monto').value = '';
-        document.getElementById('indicadores').value = '';
+        datosGuardados[index] = {
+            nombre: nombre,
+            apellido: apellido,
+            monto: monto,
+            afp: afp,
+            porcentajeafp: porcentajeafp,
+            montoReal: montoReal,
+            montoAFP: montoAFP,
+            montoFONASA: montoFONASA,
+            montoSeguro: montoSeguro
+        };
+        localStorage.setItem("datos", JSON.stringify(datosGuardados));
+        document.getElementById("nombre").value = "";
+        document.getElementById("apellido").value = "";
+        document.getElementById("monto").value = "";
+        document.getElementById("indicadores").value = "";
 
         LoadData();
-    }
-    else
+    } 
+    else 
     {
-        alert('Falta asignar valor a uno de los siguientes campos: \n 1. Nombre \n 2. Apellido \n 3. Monto \n 4. Lista de AFPs');
+        alert(
+            "Falta asignar valor a uno de los siguientes campos: \n 1. Nombre \n 2. Apellido \n 3. Monto \n 4. Lista de AFPs"
+        );
     }
 }
 
-function Clean()
-{
+function Clean() {
     localStorage.clear();
     LoadData();
 }
 
 function descargarExcel() {
-    var datosGuardados = JSON.parse(localStorage.getItem('datos')) || [];
-  
+    var datosGuardados = JSON.parse(localStorage.getItem("datos")) || [];
+
     if (datosGuardados.length === 0) {
-      alert("No hay datos guardados para descargar.");
-      return;
+        alert("No hay datos guardados para descargar.");
+        return;
     }
-  
-    var datos = [['Nombre', 'Apellido', 'Sueldo Bruto', 'AFP', 'Porcentaje AFP', 'Sueldo Liquido', 'Monto AFP', 'Monto FONASA']].concat(datosGuardados.map(function(dato) {
-      return [dato.nombre, dato.apellido, dato.monto, dato.afp, dato.porcentajeafp, dato.montoReal, dato.montoAFP, dato.montoFONASA];
-    }));
-  
+
+    var datos = [
+        [
+            "Nombre",
+            "Apellido",
+            "Sueldo Bruto",
+            "AFP",
+            "Porcentaje AFP",
+            "Sueldo Liquido",
+            "Monto AFP",
+            "Monto FONASA",
+            "Seguro de Cesantía",
+        ],
+    ].concat(
+        datosGuardados.map(function (dato) {
+            return [
+                dato.nombre,
+                dato.apellido,
+                dato.monto,
+                dato.afp,
+                dato.porcentajeafp,
+                dato.montoReal,
+                dato.montoAFP,
+                dato.montoFONASA,
+                dato.montoSeguro,
+            ];
+        })
+    );
+
     var workbook = XLSX.utils.book_new();
     var worksheet = XLSX.utils.aoa_to_sheet(datos);
     XLSX.utils.book_append_sheet(workbook, worksheet, "Datos");
-  
-    var libroBinario = XLSX.write(workbook, { bookType: 'xlsx', type: 'binary' });
-  
+
+    var libroBinario = XLSX.write(workbook, { bookType: "xlsx", type: "binary" });
+
     function s2ab(s) {
-      var buf = new ArrayBuffer(s.length);
-      var view = new Uint8Array(buf);
-      for (var i = 0; i < s.length; i++) {
-        view[i] = s.charCodeAt(i) & 0xFF;
-      }
-      return buf;
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i = 0; i < s.length; i++) {
+            view[i] = s.charCodeAt(i) & 0xff;
+        }
+        return buf;
     }
-  
-    var archivoExcel = new Blob([s2ab(libroBinario)], { type: "application/octet-stream" });
-  
+
+    var archivoExcel = new Blob([s2ab(libroBinario)], {
+        type: "application/octet-stream",
+    });
+
     var enlaceDescarga = document.createElement("a");
     enlaceDescarga.href = URL.createObjectURL(archivoExcel);
     enlaceDescarga.download = "datos.xlsx";
     enlaceDescarga.click();
-  }
-  
-  
-  
+}
